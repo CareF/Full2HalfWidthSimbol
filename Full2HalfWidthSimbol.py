40297ed0a9bfce1467ddcdeca5aaa718356492a2
L.M. (CareF) <lm0329@126.com>
2016-01-07 22:45:01 +0800

    basic realization

---
+++ Full2HalfWidthSimbol.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename: ShowTexdoc.py
+# Author:   Lyu Ming <CareF.Lm@gmail.com>
+
+import sublime, sublime_plugin
+import re
+
+def replace_in_dict(xin, dicts):
+    xout = ""
+    for c in xin:
+        xout += dicts[c]
+    return xout
+
+class SwitchCommand(sublime_plugin.TextCommand):
+    '''Switch full width simbol to half width'''
+    def run(self, edit):
+        s = sublime.load_settings("Full2HalfWidthSimbol.sublime-settings")
+        sdict_left = s.get("left")
+        sdict_right = s.get("right")
+        sdict_right.update(s.get("end"))
+        sdict_chara = s.get("character")
+        # space formatting
+        # if sdict_left:
+        #     for item in sdict_left: 
+        #         if not sdict_left[item][0].isspace(): 
+        #             sdict_left[item] = " " + sdict_left[item]
+        # if sdict_right:
+        #     for item in sdict_right: 
+        #         if not sdict_right[item][-1].isspace(): 
+        #             sdict_right[item] += " "
+        # if sdict_chara:
+        #     for item in sdict_chara: 
+        #         if not sdict_chara[item][0].isspace(): 
+        #             sdict_chara[item] = " " + sdict_chara[item]
+        #         if not sdict_chara[item][-1].isspace(): 
+        #             sdict_chara[item] += " "
+        pattern_left = re.compile(r' ?(' + '|'.join(sdict_left.keys()) + r')+')
+        pattern_right = re.compile(r'(' + '|'.join(sdict_right.keys()) + r')+ ?')
+        pattern_chara = re.compile(r'(' + '|'.join(sdict_chara.keys()) + r')+')
+
+        view = self.view
+        if view.is_read_only(): 
+            sublime.status_message("Full2HalfWidthSimbol: Cannot \
+                modify (read only)!")
+            return 
+        texts = view.substr(sublime.Region(0, view.size()))
+
+        texts = pattern_left.sub(lambda x: 
+            " "+replace_in_dict( x.group(), sdict_left), texts)
+        texts = pattern_right.sub(lambda x: 
+            replace_in_dict( x.group(), sdict_right)+" ", texts)
+        texts = pattern_chara.sub(lambda x: 
+            " "+replace_in_dict( x.group(), sdict_chara)+" ", texts)
+        view.replace(edit, sublime.Region(0, view.size()), texts)
+        sublime.status_message("Full2HalfWidthSimbol: Done!")
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Full2HalfWidthSimbol
+by Lyu Ming
+[https://github.com/CareF]
+
+## Introduction
+This is a Sublime Text package for changing punctuation of full and half width. 
+
+I develop and test the package under Sublime Text 3 and Windows 10 but I 
+shall not use anything not supported by other platform. If anyone found 
+it cannot run under Sublime Text 2 or other OS, please contact me. 
+
+## Usage
+Press `Ctrl+Shift+P` to call the panel, call the command `Full2HalfWidthSimbol: Switch`
